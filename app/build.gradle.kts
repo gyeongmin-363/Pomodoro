a12624c59c019a86461d0e9dd973283dbe5837a8
Lee Gyeongmin <lkm1282@naver.com>
2025-08-27 21:39:09 +0900

    버전 3 / 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.malrang.pomodoro"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
