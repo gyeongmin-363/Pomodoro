c4daea2e754c12929fbd1dc55577add37f8b7fec
Lee Gyeongmin <lkm1282@naver.com>
2025-09-08 23:55:24 +0900

    versionCode = 10 /  versionName = "1.1.1"

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.malrang.pomodoro"
         minSdk = 28
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
